60a14ecc11e2370afee9c1d1119d6a09ecba07ce
Matt Arnold <52244385+MattoElGato@users.noreply.github.com>
2022-10-26 15:21:33 +0100

    Initial commit

---
+++ tna-gradated-access-prototype/precache-manifest.a015424527688aee3c25b15f4f1bf9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba46eb6c34f478970a2a65836093526",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8cf3611e9e2ec648cb6",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "46bd7b5a0d575f813b00",
+    "url": "/static/css/main.a2d58aee.chunk.css"
+  },
+  {
+    "revision": "d8cf3611e9e2ec648cb6",
+    "url": "/static/js/2.54f90f43.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.54f90f43.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46bd7b5a0d575f813b00",
+    "url": "/static/js/main.e0563d91.chunk.js"
+  },
+  {
+    "revision": "0a1f673c8d40e7e00e8a",
+    "url": "/static/js/runtime-main.f26cc6bc.js"
+  }
+]);
